b82c2ef14d31621cb878888d883880dd0836d8d0
VINCENT KUSA <vincentkusa170203@gmail.com>
2024-05-07 23:48:31 +0300

    alignment

--- path.php
+++ path.php
@@ -1,5 +1,5 @@
 <?php
 define('ROOT_PATH', realpath(dirname(__FILE__)) . '/');
-define('SAP_PATH', realpath(dirname(__FILE__)) . '/sap/');
-define('OOP_PATH', realpath(dirname(__FILE__)) . '/core/oop/');
+define('SAP_PATH',  realpath(dirname(__FILE__)) . '/sap/');
+define('OOP_PATH',  realpath(dirname(__FILE__)) . '/core/oop/');
 define('FUNC_PATH', realpath(dirname(__FILE__)) . '/core/functional/');
