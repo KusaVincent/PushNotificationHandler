c661b2c220875cbecde185764434d2b01eeb61aa
Vincent.Kusa <vincentkusa170203@gmail.com>
2024-05-07 23:44:52 +0300

    alignment and validation hash

--- core/functional/build_hash.php
+++ core/functional/build_hash.php
@@ -5,8 +5,8 @@ function buildAndCompareHash(array $data, string $amount) : bool {
     $secret_key     = getEnvVariables('secret_key');
     $credit_account = getEnvVariables('credit_account');
 
-    // Hash_GeneratorForVal   = SecretKey + TransType + TransID + TransactionTime + TransAmount + BillRefNumber + MSISDN + Name
-    // Hash_GeneratorForPush  = SecretKey + TransType + TransID + TransactionTime + TransAmount + CreditAccount + BillRefNumber + MSISDN + Name + "1"
+    // Hash_GeneratorForValidation       = SecretKey + TransType + TransID + TransactionTime + TransAmount + BillRefNumber + MSISDN + Name
+    // Hash_GeneratorForPushNotification = SecretKey + TransType + TransID + TransactionTime + TransAmount + CreditAccount + BillRefNumber + MSISDN + Name + "1"
 
     if(isset($data['Name'])) { //validation
         $hash_generator = $secret_key . $data["TransType"] . $data["TransID"] . $data["TransTime"] . $data["TransAmount"] . $data["BillRefNumber"] . $data["Mobile"] . $data['Name']; 
