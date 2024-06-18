063ddf73d8e7d957e90be2201887917796f56621
VINCENT KUSA <vincentkusa170203@gmail.com>
2024-06-18 21:15:58 +0300

    http respose code array

--- push_api/functional/json.php
+++ push_api/functional/json.php
@@ -6,8 +6,15 @@ function json_response(int $code = 200, array $message) : string
     http_response_code($code);
     header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
     header('Content-Type: application/json');
-    
-    header('Status: '. $code);
+
+    $status = [
+        200 => 'OK',
+        400 => 'Bad Request',
+        403 => 'Forbidden',
+        401 => 'Unauthorized'
+    ]
+
+    header('Status: '. $status[$code]);
 
     return json_encode($message);
 }
