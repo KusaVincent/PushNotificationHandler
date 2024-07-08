aeb0664b5626fbc96f7ca2d1ca63f1211dcbddfe
KusaVincent <vincentkusa170203@gmail.com>
2024-07-08 23:27:35 +0300

    log sms

--- alerts/sms/index.php
+++ alerts/sms/index.php
@@ -2,20 +2,22 @@
 require_once __DIR__ . '/../../push_api/functional/load_files.php';
 require_once SMS_ALERT_PATH . 'SMS.php';
 
-$smsFileEntries = HandleCSV::readCSV(getEnvVariables('msg_arc_file'));
+try {
+    $smsFileEntries = HandleCSV::readCSV(getEnvVariables('msg_arc_file'));
 
-if (empty($smsFileEntries))
-    return false;
+    if (empty($smsFileEntries))
+        return false;
 
+    foreach ($smsFileEntries as $smsFileEntry => $smsFileData) {
+        $smsEntry = linearSearch($smsFileData[0], HandleCSV::readCSV(getEnvVariables('sms_file')), true);
 
-foreach ($smsFileEntries as $smsFileEntry => $smsFileData) {
-    $smsEntry = linearSearch($smsFileData[0], HandleCSV::readCSV(getEnvVariables('sms_file')), true);
+        $smsBody = "Payment for $smsEntry[0] of KES $smsEntry[3] received from $smsEntry[1] at $smsEntry[4].Mpesa reference: $smsEntry[2].";
 
-    $smsBody = "Payment for $smsEntry[0] of KES $smsEntry[3] received from $smsEntry[1] at $smsEntry[4].Mpesa reference: $smsEntry[2].";
-
-    echo (new SMS($smsFileData[1], $smsBody))->send();
+        logThis(4, (new SMS($smsFileData[1], $smsBody))->send());
+    }
+} catch (Exception $e) {
+    logThis(2, "An error occurred: " . $e->getMessage() . "\n" . $e);
 }
-
 // mv sms.csv sms_archive.csv
 
 // php smsindexfile.csv
