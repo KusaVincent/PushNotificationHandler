553a748c4a17252d765ca74dfc4eb9daeb04948f
KusaVincent <vincentkusa170203@gmail.com>
2024-07-24 19:25:09 +0300

    add authentication toggle

--- alerts/sms/index.php
+++ alerts/sms/index.php
@@ -3,13 +3,13 @@ require_once __DIR__ . '/../../push_api/functional/load_files.php';
 require_once SMS_ALERT_PATH . 'SMS.php';
 
 try {
-    $smsFileEntries = HandleCSV::readCSV(getEnvVariables('msg_arc_file'));
+    $smsFileEntries = HandleCSV::readCSV(getEnvVariables('sms_file'));
 
     if (empty($smsFileEntries))
         return false;
 
     foreach ($smsFileEntries as $smsFileEntry => $smsFileData) {
-        $smsEntry = linearSearch($smsFileData[0], HandleCSV::readCSV(getEnvVariables('sms_file')), true);
+        $smsEntry = linearSearch($smsFileData[0], HandleCSV::readCSV(getEnvVariables('msg_arc_file')), true);
 
         $smsBody = "Dear $smsFileData[2],";
         $smsBody .= "Payment for $smsEntry[0] of KES $smsEntry[3] received from $smsEntry[1] at $smsEntry[4].Mpesa reference: $smsEntry[2].";
@@ -19,8 +19,3 @@ try {
 } catch (Exception $e) {
     logThis(2, "An error occurred: " . $e->getMessage() . "\n" . $e);
 }
-// mv sms.csv sms_archive.csv
-
-// php smsindexfile.csv
-
-// rm -rf sms_archive.csv
